50eecad6a94e87600f7865882c2730aafecb8d42
aosk <alshaghdali@Abdulrahmans-MacBook-Pro.local>
2017-02-20 05:59:52 +0000

    20th Feb
    This is another update

--- Android/V2/letsUnite/app/src/main/java/com/example/abdulrahmanalshaghdali/letsunite/LoginActivity.java
+++ Android/V2/letsUnite/app/src/main/java/com/example/abdulrahmanalshaghdali/letsunite/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.abdulrahmanalshaghdali.letsunite;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.http.params.HttpConnectionParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,24 +29,29 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import static android.R.id.message;
+import static java.net.Proxy.Type.HTTP;
+
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity
-{
+public class LoginActivity extends AppCompatActivity {
 
-
+    String data = "";
 
     // UI references.
     private EditText mEmailView;
     private EditText mPasswordView;
-    private Button singinBTN ;
-    private Button signupBTN ;
-
+    private Button singinBTN;
+    private Button signupBTN;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -46,31 +60,31 @@ public class LoginActivity extends AppCompatActivity
         // password
         mPasswordView = (EditText) findViewById(R.id.password);
         singinBTN = (Button) findViewById(R.id.email_sign_in_button);
-        singinBTN.setOnClickListener(new View.OnClickListener()
-        {
+        singinBTN.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
+
+                String server = "http://54.191.242.216:8000/";
+                String local = "http://10.0.2.2:8000/login/";
 
                 JSONObject postData = new JSONObject();
                 try {
                     postData.put("email", mEmailView.getText().toString());
                     postData.put("password", mPasswordView.getText().toString());
 
-                    Toast.makeText(LoginActivity.this, postData.toString(),
-                            Toast.LENGTH_LONG).show();
+                    //Toast.makeText(LoginActivity.this, postData.toString() + "Data sent",
+                    //        Toast.LENGTH_LONG).show();
 
 
-                    SendDeviceDetails sa = new SendDeviceDetails();//("http://127.0.0.1:8000/login/", postData.toString())
-                    sa.execute("http://54.191.242.216:8080/", postData.toString());
+                    //SendDeviceDetails sa = new SendDeviceDetails();//("http://127.0.0.1:8000/login/", postData.toString())
+                    new LoginActivity.SendDeviceDetails().execute(local, postData.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                     Toast.makeText(LoginActivity.this, "login failed 2 ==)",
                             Toast.LENGTH_LONG).show();
                 }
-
 
 
 
@@ -93,56 +107,95 @@ public class LoginActivity extends AppCompatActivity
             }
         });
         signupBTN = (Button) findViewById(R.id.signUp);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+
 
     private class SendDeviceDetails extends AsyncTask<String, Void, String> {
 
+
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(final String... params) {
 
-            String data = "";
 
-            HttpURLConnection httpURLConnection = null;
-            try {
+            Thread t = new Thread() {
 
-                httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
-                httpURLConnection.setRequestMethod("POST");
 
-                httpURLConnection.setDoOutput(true);
 
-                DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
-                wr.writeBytes("PostData=" + params[1]);
-                wr.flush();
-                wr.close();
+                public void run() {
+                    Looper.prepare(); //For Preparing Message Pool for the child Thread
+                    String data = "";
 
-                InputStream in = httpURLConnection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(in);
+                    HttpURLConnection httpURLConnection = null;
 
-                int inputStreamData = inputStreamReader.read();
-                while (inputStreamData != -1) {
-                    char current = (char) inputStreamData;
-                    inputStreamData = inputStreamReader.read();
-                    data += current;
+
+
+                    try {
+
+
+
+                        httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+                        httpURLConnection.setRequestMethod("POST");
+
+                        httpURLConnection.setDoOutput(true);
+
+                        DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+                        wr.writeBytes("PostData=" + params[1]);//----------------------- Check point --!
+                        Toast.makeText(LoginActivity.this, params[1] + "Data sent",
+                                Toast.LENGTH_LONG).show();
+
+                        wr.flush();
+                        wr.close();
+
+                        /*
+                        InputStream in = httpURLConnection.getInputStream();
+                        InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+                        int inputStreamData = inputStreamReader.read();
+                        while (inputStreamData != -1) {
+                            char current = (char) inputStreamData;
+                            inputStreamData = inputStreamReader.read();
+                            data += current;
+                        }*/
+
+                        InputStream in = httpURLConnection.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                        StringBuilder result = new StringBuilder();
+                        String line;
+                        while((line = reader.readLine()) != null)
+                        {
+                            result.append(line);
+                        }
+                        System.out.println(result.toString());
+
+
+
+                    /*Checking response */
+                        if (httpURLConnection != null) {
+                            httpURLConnection.disconnect();
+                            Toast.makeText(LoginActivity.this, "Connection disconnected",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(LoginActivity.this, "Cannot Estabilish Connection",
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    Looper.loop(); //Loop in the message queue
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+            };
 
-                Toast.makeText(LoginActivity.this, "login failed: ",
-                        Toast.LENGTH_LONG).show();
-            } finally {
-                if (httpURLConnection != null) {
-                    httpURLConnection.disconnect();
-                }
-            }
-
+            t.start();
             return data;
+
         }
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            //Log.e("TAG", result); // this is expecting a response code to be sent from your server upon receiving the POST data
-        }
     }
 
 }
